a76d6645a87975f6edc3d58a391ee80c8d13f7cd
R0rs12ach@gmail.com <R0rs12ach@gmail.com>
2016-07-20 09:31:37 +0800

    delete files which unessary

--- auto_report/auto_report.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-SENDDATE=`date +%Y%m%d`彭岁万工作周报
-echo $SENDDATE
-/usr/bin/python /root/peakmonkey/auto_report/doc.py
-/bin/mail -s "邮件主题 - $SENDDATE" -c "pengsuiwan@daowoo.com" -a /root/peakmonkey/auto_report/彭岁万的工作周报.docx ued@daowoo.com < /root/peakmonkey/auto_report/content.txt
